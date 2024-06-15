5433547c45ff7c8c50a2110309c682b6c720484f
Azizbek Rustamov <rystamov2233@gmail.com>
2024-06-15 13:32:45 +0500

    amaliyot

---
+++ index.js
@@ -0,0 +1,62 @@
+// let son1 = 5;
+// let son2 = 7;
+
+// console.log(son1 + son2); 
+
+
+// 2 - vazifa
+// let a = "15";
+// let b = Number(a);
+
+// console.log(b);
+
+
+
+// let random = Math.floor(Math.random()*100);
+
+// if (random % 2 === 0) {
+//    console.log(`${random} -> juft son`);
+// } else {
+//     console.log(`${random} -> toq son`);
+// }
+
+
+
+
+
+// let gap = prompt("soz kiriting");
+
+// if (gap === "mars") {
+//     alert("mars");
+// } else {
+//     alert("mars it school");
+// }
+
+
+
+
+// let narsalar = ["aziz", "toxir", "ma'murjon", "mir kimil", "husan"];
+// let katta5 = [];
+// let kichkina5 = [];
+
+// for (let narsa of narsalar) {
+//     if (narsa.length >= 5) {
+//         katta5.push(narsa);
+//     } else {
+//         kichkina5.push(narsa);
+//     }
+// }
+
+// console.log(katta5);
+// console.log(kichkina5)
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} -> jift son`)
+    } else {
+        console.log(`${i} -> toq son`)
+    }
+}
+
+
